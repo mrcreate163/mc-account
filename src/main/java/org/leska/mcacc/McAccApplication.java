6649995ea297d67cd5402e4a5149b39969369e68
Aleksei <aleksei.rvachev@mail.ru>
2025-11-15 13:53:32 +0300

    initial commit

---
+++ src/main/java/org/leska/mcacc/McAccApplication.java
@@ -0,0 +1,13 @@
+package org.leska.mcacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McAccApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McAccApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/leska/mcacc/McAccApplicationTests.java
@@ -0,0 +1,13 @@
+package org.leska.mcacc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McAccApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
