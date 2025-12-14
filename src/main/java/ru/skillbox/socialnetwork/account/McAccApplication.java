99dbb4daafcdbabafea0c10646b388608e940993
aleksei <aleksei.rvachev@mail.ru>
2025-12-15 00:00:01 +0300

    add eureka application.yml

--- src/main/java/ru/skillbox/socialnetwork/account/McAccApplication.java
+++ src/main/java/ru/skillbox/socialnetwork/account/McAccApplication.java
@@ -2,12 +2,12 @@ package ru.skillbox.socialnetwork.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class McAccApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(McAccApplication.class, args);
     }
-
 }
