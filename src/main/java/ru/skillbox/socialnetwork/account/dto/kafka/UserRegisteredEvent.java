c106e8b0f6085e0c9fbddc1e528ee438ad0f27e7
aleksei <aleksei.rvachev@mail.ru>
2025-12-21 18:59:27 +0300

    Update event registered

--- src/main/java/ru/skillbox/socialnetwork/account/dto/kafka/UserRegisteredEvent.java
+++ src/main/java/ru/skillbox/socialnetwork/account/dto/kafka/UserRegisteredEvent.java
@@ -1,6 +1,5 @@
 package ru.skillbox.socialnetwork.account.dto.kafka;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
